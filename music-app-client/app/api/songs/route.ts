17055ec234f4365031a5c873561bfcfeaaddeb9f
abhinav khanduja <abhinavkhanduja@abhinavs-MacBook-Pro.local>
2025-03-31 01:32:58 +0530

    wip

--- music-app-client/app/api/songs/route.ts
+++
@@ -1,9 +0,0 @@
-export const revalidate = 60;
-
-export async function GET(request: Request) {
-  const url = process.env.BASE_URL + "/songs";
-  const data = await fetch(url);
-  const songs = await data.json();
-
-  return Response.json(songs);
-}
